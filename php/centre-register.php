5c72fb286f435e2c6cf3ccb9dc8d2e9ad7c59b88
Dharneesh B <dharneesh.ec20@bitsathy.ac.in>
2023-04-21 09:38:01 +0530

    Update centre-register.php

--- php/centre-register.php
+++ php/centre-register.php
@@ -144,7 +144,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
                 <div class="col-sm-6 d-flex justify-content-end">
-                    <a class="btn btn-outline-primary" href="/covid/php/index.php">Cancel</a>
+                    <a class="btn btn-outline-primary" href="/php/index.php">Cancel</a>
                 </div>
                 </div>
 
